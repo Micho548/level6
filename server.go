aa96a972c206dd3fe2d8f44797f7f75d0583a5bb
Mijael Balderrama Herrera <micho_548@hotmail.com>
2021-10-22 16:02:55 -0400

    server

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/Micho548/level6/chat/chat/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
